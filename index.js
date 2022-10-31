8cb9873aac9c80df8d5129e2965b4bdd835ce000
Prawito Hudoro <prawitohudoro@gmail.com>
2022-10-31 07:31:40 +0700

    Basic Routing

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require('express');
 
 const app = express();
 
+// app.method(path, handler);
+// app.use("/", (req, res, next) => {
+//     res.send('Hello World');
+// });
+
+app.get("/", (req, res) => {
+    res.send('Hello Get Method');
+});
+
+app.post("/", (req, res) => {
+    res.send('Hello POST Method');
+});
+
+
+
 app.listen(4000, () => {
     console.log('Server berhasil di running di port 4000');
 })
