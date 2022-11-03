77b13985bd6a84b265d29d03766260eeef212052
Prawito Hudoro <prawitohudoro@gmail.com>
2022-11-04 02:24:54 +0700

    create connection to mysql

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const usersRoutes = require('./routes/users');
 
 const middlewareLogRequest = require('./middleware/logs');
+
+const dbPool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'express_mysql',
+});
 
 const app = express();
 
@@ -10,6 +18,21 @@ app.use(middlewareLogRequest);
 app.use(express.json());
 
 app.use('/users', usersRoutes);
+
+app.use('/', (req, res) => {
+    dbPool.execute('SELECT * FROM users', (err, rows) => {
+        if(err){
+            res.json({
+                message: 'connection failed'
+            })
+        }
+
+        res.json({
+            message: 'connection success',
+            data: rows,
+        })
+    })
+})
 
 app.listen(4000, () => {
     console.log('Server berhasil di running di port 4000');
